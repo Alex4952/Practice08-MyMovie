9d2fc87af21911940b879db5e57412f6c6f532df
Mac <Mac@macui-imac-6.local>
2015-11-14 13:02:34 +0900

    1

---
+++ MyMovie2/MovieCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCell.swift
+//  MyMovie2
+//
+//  Created by Mac on 2015. 11. 14..
+//  Copyright © 2015년 Mac. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell : UITableViewCell {
+	
+	@IBOutlet var title: UILabel!
+	@IBOutlet var desc: UILabel!
+	@IBOutlet var rating: UILabel!
+	
+}
---
+++ MyMovie2/MovieVO.swift
@@ -0,0 +1,19 @@
+//
+//  MovieVO.swift
+//  MyMovie2
+//
+//  Created by Mac on 2015. 11. 14..
+//  Copyright © 2015년 Mac. All rights reserved.
+//
+
+import UIKit
+
+class MovieVO {
+	var title : String?
+	var description : String?
+	var detail : String?
+	var rating : Float?
+	var thunbnail : String?
+	var thumbnailImage : UIImage?
+}
+
