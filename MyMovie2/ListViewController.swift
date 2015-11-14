df6558bc749f7c2983955de229b50b87bcda0943
Mac <Mac@macui-imac-6.local>
2015-11-14 14:42:47 +0900

    목록에 뿌리기까지 완성

--- MyMovie2/ListViewController.swift
+++ MyMovie2/ListViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ListViewController : UITableViewController {
 	
+	@IBOutlet var tv: UITableView!
 	var list = [MovieVO]()
 //	var list = Array<MovieVO>()
 	
@@ -18,7 +19,7 @@ class ListViewController : UITableViewController {
 		let urlStrig = "http://apis.skplanetx.com/hoppin/movies?order=releasedateasc&count=10&page=1&genreId=&version=1&appKey=ba8997a8-a439-398f-af06-d354b61d249e"
 		let apiURI = NSURL(string: urlStrig)
 		
-		// 2. REST API 호출 (NSData가 내부적으로 호출하는 기능 있음, NSURLRequest도 가능)
+		// 2. REST API 호출 (NSData가 내부적으로 호출하는 기능 있음, NSURLRequest/NSMutableURLRequest도 가능)
 		// 포스트방식은 NSURLRequest를 이용
 		let apidata :  NSData? = NSData(contentsOfURL: apiURI!)
 		
@@ -32,8 +33,11 @@ class ListViewController : UITableViewController {
 			
 			// 5. 데이터 구조에 따라 차례대로 캐스팅하며 읽어온다.
 			let hoppin = result["hoppin"] as! NSDictionary
+			let totalCount = hoppin["totalCount"]
 			let movies = hoppin["movies"] as! NSDictionary
 			let movie = movies["movie"] as! NSArray
+			
+			print("+++++ totalCount: \(totalCount)")
 			
 			// 6. 테이블 뷰 리스트를 구성할 데이터 형식
 			var mvo : MovieVO
@@ -47,6 +51,8 @@ class ListViewController : UITableViewController {
 				mvo.thumbnail = row["thumbnailImage"] as? String
 				mvo.detail = row["linkUrl"] as? String
 				mvo.rating = (row["ratingAverage"] as? NSString)!.floatValue
+				
+				self.list.append(mvo)
 			}
 			
 		} catch {
@@ -56,6 +62,27 @@ class ListViewController : UITableViewController {
 
 	override func viewDidLoad() {
 		callMovieAPI()
+		
+		// 화면에 다 뿌린 다음 (indexPath를 확인 한 후에 원하는 위치로 이동)
+		// 테이블뷰 를 아울렛 변수로 추가
+//		tv.scrollToRowAtIndexPath(<#T##indexPath: NSIndexPath##NSIndexPath#>, atScrollPosition: <#T##UITableViewScrollPosition#>, animated: <#T##Bool#>)
 	}
+	
+	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return self.list.count
+	}
+
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let mvo = self.list[indexPath.row]
+		let cell = tableView.dequeueReusableCellWithIdentifier("cell") as! MovieCell
+		
+		cell.title?.text = mvo.title
+		cell.desc?.text = mvo.description
+		cell.rating?.text = String(mvo.rating!)
+		
+		return cell
+	}
+	
+
 	
 }
